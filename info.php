5cbb6598280ff7c839460dc02fd6cd6f74fa8350
Kat2 <main@kat2.net>
2016-11-10 10:39:07 +1100

    0.0.1

--- info.php
+++ info.php
@@ -3,7 +3,15 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
 $info = array(
-        'v' => 1
+    'v' => 0.0.1,
+    'list' => array(
+        'active' => false,
+        'id' => 0,
+        'domains_done' => 0,
+        'domains_left' => 0,
+        'domains_total' => 0,
+        'percentage' => 0
+    )
 );
 
 echo json_encode($info, JSON_PRETTY_PRINT);
