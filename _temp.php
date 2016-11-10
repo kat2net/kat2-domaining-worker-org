68a19fbd6efa99462f112f7d77689712e4d76baf
Kat2 <main@kat2.net>
2016-11-10 11:09:38 +1100

    _temp

--- _temp.php
+++ _temp.php
@@ -1,9 +1,21 @@
 <?php
 
-$fp = @fsockopen('whois.publicinterestregistry.net', 43, $errno, $errstr, 10);
-fputs($fp, '8st.org' . "\r\n");
-$out = "";
-while(!feof($fp)){$out .= fgets($fp);}
-fclose($fp);
+$domains = array();
 
-echo $out;
+$range = range(0, 1);
+foreach($range as $r){
+    $domains[] = $r.'st.org';
+}
+
+foreach($domains as $domain){
+    echo $domain."\n\n";
+    $fp = @fsockopen('whois.publicinterestregistry.net', 43, $errno, $errstr, 10);
+    fputs($fp, $domain . "\r\n");
+    $out = "";
+    while(!feof($fp)){$out .= fgets($fp);}
+    fclose($fp);
+
+    echo $out;
+
+    echo "\n\n";
+}
